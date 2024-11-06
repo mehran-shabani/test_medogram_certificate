2c3f2e79ace4f5b7c767061f0406d10a62a15869
Dr.hossein shabani (Mehran) <shabanimehran@gmail.com>
2024-11-06 11:40:51 +0330

    merge files

--- certificate.py
+++ certificate.py
@@ -8,7 +8,12 @@ import jdatetime
 import io
 from PIL import Image
 
-from create_binary_of_image import binary_generator
+def binary_generator():
+    file_path = 'logo.png'
+    with open(file_path, "rb") as image_file:
+        binary_data = image_file.read()
+    return binary_data
+
 
 def reshape_text(text):
     return get_display(arabic_reshaper.reshape(text))
--- create_binary_of_image.py
+++
@@ -1,7 +0,0 @@
-def binary_generator():
-    file_path = 'logo.png'
-    with open(file_path, "rb") as image_file:
-        binary_data = image_file.read()
-    return binary_data
-
-
